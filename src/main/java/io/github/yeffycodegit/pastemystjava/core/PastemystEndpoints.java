65fca4d47478f1fda9ef2fb7a1ba0587d0ba96ef
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-07-07 15:10:54 +0600

    Update LICENSE.txt

---
+++ src/main/java/io/github/yeffycodegit/pastemystjava/core/PastemystEndpoints.java
@@ -0,0 +1,8 @@
+package io.github.yeffycodegit.pastemystjava.core;
+
+public class PastemystEndpoints {
+    public static final String PASTE_ENDPOINT = PastemystApi.BASE_ENDPOINT + PastemystApi.PASTE;
+    public static final String DATA_ENDPOINT = PastemystApi.BASE_ENDPOINT + PastemystApi.DATA;
+    public static final String TIME_ENDPOINT = PastemystApi.BASE_ENDPOINT + PastemystApi.TIME;
+    public static final String USER_ENDPOINT = PastemystApi.BASE_ENDPOINT + PastemystApi.USER;
+}
